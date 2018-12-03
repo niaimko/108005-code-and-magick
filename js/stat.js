7123a4440658a2c96b79f94139932f8ae0e45bb1
Няим <niaimko@gmail.com>
2018-12-03 17:14:28 +0300

    Улучшает экран успешного прохождения уровня

---
+++ js/stat.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 16;
+var COLUMN_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var columnHeight = -(BAR_HEIGHT - FONT_GAP - GAP - GAP - FONT_GAP); // высота шкалы в гистограмме
+var COLUMN_GAP = 50;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+  return maxElement;
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+};
+
+// функция для получения случайного целого числа в заданном диапазоне
+var getRandomInteger = function(min, max) {
+  var randomInteger = min - 0.5 + Math.random() * (max - min + 1);
+  randomInteger = Math.round(randomInteger);
+  return randomInteger;
+};
+
+window.renderStatistics = function(ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + GAP + GAP);
+
+  ctx.textBaseline = 'alphabetic';
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var saturationIndex = getRandomInteger(0, 100);
+      ctx.fillStyle = 'hsl(240,' + saturationIndex + '%, 50%)';
+    }
+
+    ctx.fillRect(CLOUD_X + GAP * 3 + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - GAP - FONT_GAP - GAP, COLUMN_WIDTH, (columnHeight * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * 3 + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - GAP - FONT_GAP - GAP - GAP + (columnHeight * times[i]) / maxTime);
+    ctx.fillText(players[i], CLOUD_X + GAP * 3 + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - GAP);
+  }
+};
